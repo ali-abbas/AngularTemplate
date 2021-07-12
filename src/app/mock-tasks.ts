8d675f7c85723579c60df045c1584f4c9ae18cef
Syed Ali <ali_abbas89@hotmail.com>
2021-07-12 11:15:27 +1000

    Intial Commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,19 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'May 5th at 2.30pm',
+        reminder : true 
+    },
+    {
+        id: 2,
+        text: 'Vic roads appointment',
+        day: 'May 5th at 2.30pm',
+        reminder : true 
+    }
+
+
+];
